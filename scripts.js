00359c8c42b3e736d9d2fac11f234de9bdb51376
ItsNiick <nshedd99@csu.fullerton.edu>
2024-04-09 23:57:53 -0700

    Submission Deadline
    I was unable to fully implement the features I wanted, as I ran out of time since I had multiple projects the week of this project.

--- scripts.js
+++ scripts.js
@@ -1,44 +1,130 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const WARRIOR_URL = "https://hearthstone.wiki.gg/images/e/e4/HERO_01.png"
+const SHAMAN_URL = "https://hearthstone.wiki.gg/images/5/51/HERO_02.png"
+const ROGUE_URL  = "https://hearthstone.wiki.gg/images/1/1f/HERO_03.png"
+const PALADIN_URL = "https://hearthstone.wiki.gg/images/e/e7/HERO_04.png"
+const HUNTER_URL = "https://hearthstone.wiki.gg/images/8/8b/HERO_05.png"
+const DRUID_URL = "https://hearthstone.wiki.gg/images/e/e2/HERO_06.png"
+const WARLOCK_URL = "https://hearthstone.wiki.gg/images/8/84/HERO_07.png"
+const MAGE_URL = "https://hearthstone.wiki.gg/images/a/af/HERO_08.png"
+const PRIEST_URL = "https://hearthstone.wiki.gg/images/b/b1/HERO_09.png"
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelector('.menu-li');
+    const selected = dropdown.querySelector('.selected');
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+    options.forEach(option =>  {
+        option.addEventListener('change', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add();
+        });
+    });
+});
+
+let player_class = []
+let name = []
+let cost = []
+let attack = []
+let health = []
+let type = []
+let rarity = []
+
+fs.createReadStream('cards_flat.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+        player_class.push(row.player_class);
+        name.push(row.name);
+        cost.push(row.cost);
+        attack.push(row.attack);
+        health.push(row.health);
+        type.push(row.type);
+        rarity.push(row.rarity);
+    })
+    .on('end', () => {
+        // Count occurrences of each cost value
+        const costCounts = {};
+        cost.forEach((value) => {
+                costCounts[value] = (costCounts[value] || 0) + 1;
+        });
+
+        // Output the counts for each cost value
+        console.log('Cost Counts:');
+        for (const [value, count] of Object.entries(costCounts)) {
+            if (value !== '') {
+            console.log(`Cost ${value}: ${count} entries`);
+            }
+            if (value == '') {
+                console.log(`Free Summon: ${count} entries`);
+            }
+        }
+
+        // Count occurences of each class value
+        const classCounts = {};
+        player_class.forEach((value) => {
+            // Exclude specific class values from counting
+            if (value !== '' && value !== 'DREAM' && value !== 'DEATHKNIGHT') {
+                classCounts[value] = (classCounts[value] || 0) + 1;
+            }
+        });
+
+        // Count occurrences of each attack value
+        const attackCounts = {};
+        attack.forEach((value) => {
+                attackCounts[value] = (attackCounts[value] || 0) + 1;
+        });
+    
+        // Count occurrences of each health value
+        const healthCounts = {};
+        health.forEach((value) => {
+            healthCounts[value] = (healthCounts[value] || 0) + 1;
+        });
 
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+        // Count occurrences of each type value
+        const typeCounts = {};
+        type.forEach((value) => {
+            typeCounts[value] = (typeCounts[value] || 0) + 1;
+        });
+        
+        // Count occurrences of each type value
+        const rarityCounts = {};
+        rarity.forEach((value) => {
+            rarityCounts[value] = (rarityCounts[value] || 0) + 1;
+        });
 
-// This is an array of strings (TV show titles)
+    });
+
+
 let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+    "Warrior",
+    "Shaman",
+    "Rogue",
+    "Paladin",
+    "Hunter",
+    "Druid",
+    "Warlock",
+    "Mage",
+    "Priest"
 ];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
 
-
-// This function adds cards the page to display the data in the array
+// This function displays the associated class image for the given class name.
 function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
@@ -46,17 +132,26 @@ function showCards() {
     
     for (let i = 0; i < titles.length; i++) {
         let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
         if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
+            imageURL = WARRIOR_URL;
         } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
+            imageURL = SHAMAN_URL;
         } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
+            imageURL = ROGUE_URL;
+        } else if (i == 3) {
+            imageURL = PALADIN_URL;
+        } else if (i == 4) {
+            imageURL = HUNTER_URL;
+        } else if (i == 5) {
+            imageURL = DRUID_URL;
+        } else if (i == 6) {
+            imageURL = WARLOCK_URL;
+        } else if (i == 7) {
+            imageURL = MAGE_URL;
+        } else if (i == 8) {
+            imageURL = PRIEST_URL;
         }
+            
 
         const nextCard = templateCard.cloneNode(true); // Copy the template card
         editCardContent(nextCard, title, imageURL); // Edit title and image
@@ -74,21 +169,8 @@ function editCardContent(card, newTitle, newImageURL) {
     cardImage.src = newImageURL;
     cardImage.alt = newTitle + " Poster";
 
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
     console.log("new card:", newTitle, "- html: ", card);
 }
 
 // This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
-}
-
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
-}
